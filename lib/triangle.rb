6457ece410796db210ba612469cbd309b34f5faa
Eden Mesfun <emesfun@gmail.com>
2018-02-16 16:44:01 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,6 +14,7 @@ class Triangle
               raise TriangleError
             elsif side1 <= -1 || side2 <= -1 || side3 <= -1
               raise TriangleError
+              
             elsif side1 == side2 && side2 == side3
               return :equilateral
             elsif
