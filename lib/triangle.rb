5abccd6b5b4d45c6764ec672fdf38e8930d2f2d5
Eden Mesfun <emesfun@gmail.com>
2018-02-16 16:41:39 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,8 @@ class Triangle
       end
 
       def kind
-            if
+            if side1 == 0 && side2 == 0 && side3 ==0
+
             if side1 == side2 && side2 == side3
               return :equilateral
             elsif
