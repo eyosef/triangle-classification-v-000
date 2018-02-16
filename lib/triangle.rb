8b501b4e2fee793559b7aa479ab8aa695eda331a
Eden Mesfun <emesfun@gmail.com>
2018-02-16 15:45:54 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,9 +10,6 @@ class Triangle
       end
 
       def kind
-        # side1.self = side1
-        # side2.self = side2
-        # side3.self = self3
 
         if side1 != side2 && side2 != side3
           begin
