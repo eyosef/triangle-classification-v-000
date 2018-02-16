fbc60f0a77f3df660a5eeea98e97647818d03166
Eden Mesfun <emesfun@gmail.com>
2018-02-16 16:35:00 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,15 +11,15 @@ class Triangle
 
       def kind
         begin
-          if side1 == side2 && side2 == side3
-            return :equilateral
-          elsif
-            side1 == side2 || side2 == side3 || (side1 == side3)
-            return :isosceles
-          elsif
-            side1 != side2 && side2 != side3
-            return :scalene
-          end #if statement
+            if side1 == side2 && side2 == side3
+              return :equilateral
+            elsif
+              side1 == side2 || side2 == side3 || (side1 == side3)
+              return :isosceles
+            elsif
+              side1 != side2 && side2 != side3
+              return :scalene
+            end #if statement
         raise TriangleError
         rescue TriangleError => error
         puts error.notice
