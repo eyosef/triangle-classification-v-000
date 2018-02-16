3432bf4583a38285e3270bb35147a29f063d5d05
Eden Mesfun <emesfun@gmail.com>
2018-02-16 16:30:55 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,6 +13,11 @@ class Triangle
         begin
           if side1 == side2 && side2 == side3
             return :equilateral
+          end #if statement 
+        end #begin 
+      end #kind method
+
+            
       def kind
             if side1 == side2 && side2 == side3
               return :equilateral
