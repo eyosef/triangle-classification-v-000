e7368b3574eb28723520a001e5e3bc0129076dd6
Eden Mesfun <emesfun@gmail.com>
2018-02-16 16:33:30 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -19,19 +19,6 @@ class Triangle
         puts error.notice
         end #begin
       end #kind method
-
-
-      def kind
-            if side1 == side2 && side2 == side3
-              return :equilateral
-            elsif
-              side1 == side2 || side2 == side3 || (side1 == side3)
-              return :isosceles
-            elsif
-              side1 != side2 && side2 != side3
-              return :scalene
-            end #if statement
-      end #kind method
 end #Triangle class
 
 class TriangleError < StandardError
